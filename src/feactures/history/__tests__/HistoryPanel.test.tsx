85b35cbdc4cb16726714af1a7532348477466c3f
omarqui <omarquiaybar@gmail.com>
2021-01-22 23:03:12 -0400

    test: testing library first implemnt
    create renderWithState helper
    add tests to HistoryPanel

---
+++ src/feactures/history/__tests__/HistoryPanel.test.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { fireEvent, render, screen, store } from "../../../utils/tests_utils";
+import HistoryPanel from "../HistoryPanel";
+import * as db from '../../../dataServices/SearchHistory';
+import { IDictonaryData } from "../../../types";
+import moment from "moment";
+import { setCurrentDefinition } from "../../search/actions";
+
+const expectedDate = new Date("2017-12-19")
+const expectedWord = {
+    meanings: [],
+    phonetics: [],
+    word: "soy una palabra",
+    date: expectedDate
+};
+const expectedHistory: Array<IDictonaryData> = [expectedWord];
+
+function spyHistory() {
+    const spyGetHistory = jest.spyOn(db, "getHistory");
+    spyGetHistory.mockResolvedValue(expectedHistory);
+}
+it("should render HistoryPanel component with info", async () => {
+    spyHistory();
+    const dateFormmated = moment(expectedDate).format("YYYY-MM-DD hh:mm A")
+    render(<HistoryPanel />)
+    const wordElement = await screen.findByText(new RegExp(expectedWord.word))
+    const dateElement = await screen.findByText(new RegExp(dateFormmated))
+
+    expect(wordElement).toBeInTheDocument();
+    expect(dateElement).toBeInTheDocument();
+})
+
+it("should dispatch SET_CURRENT_DEFINITION action when click word element", async () => {
+    const spyStoreDispatch = jest.spyOn(store, "dispatch")
+    spyHistory()
+
+    render(<HistoryPanel />)
+    const wordElement = await screen.findByText(new RegExp(expectedWord.word))
+    fireEvent.click(wordElement);
+    expect(spyStoreDispatch).toHaveBeenCalled();
+    expect(spyStoreDispatch).toHaveBeenLastCalledWith(setCurrentDefinition(expectedWord))
+})
---
+++ src/utils/tests_utils.tsx
@@ -0,0 +1,23 @@
+import React, { FunctionComponent, ReactElement } from "react";
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from 'redux-thunk';
+import rootReducer from "../redux/rootReducer";
+
+const store = createStore(
+    rootReducer,
+    applyMiddleware(thunk)
+);
+
+const Wrapper: FunctionComponent = ({ children }) => (
+    <Provider store={store} >
+        { children}
+    </Provider>
+)
+
+const renderWithState = (ui: ReactElement, options?: Object) =>
+    render(ui, { wrapper: Wrapper, ...options })
+
+export * from '@testing-library/react'
+export { renderWithState as render, store }
